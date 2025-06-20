33ad1c77c8a9b066b048c5246201d3430fce30f6
shohibunnajam <shohibunnajam@gmail.com>
2025-06-20 18:35:05 +0700

    new project

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="w-screen h-full flex justify-center items-center bg-blue-600">
+      <div className="w-4/12 border-2 rounded-lg shadow-lg bg-white">
+        <h1>Login</h1>
+      </div>
+    </div>
+  );
+}
